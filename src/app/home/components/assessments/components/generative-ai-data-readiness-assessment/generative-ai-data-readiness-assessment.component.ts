6dca302cde74ec898208613639e0ad6ea97b7275
Dharam <speak2dharam@gmail.com>
2024-04-04 11:14:01 +0530

    meta tag update

--- src/app/home/components/assessments/components/generative-ai-data-readiness-assessment/generative-ai-data-readiness-assessment.component.ts
+++ src/app/home/components/assessments/components/generative-ai-data-readiness-assessment/generative-ai-data-readiness-assessment.component.ts
@@ -11,5 +11,23 @@ export class GenerativeAiDataReadinessAssessmentComponent {
     this.titleSrc.setTitle('Generative AI Data Readiness Assessment Services - UnifyCloud');
     this.meta.addTag({name:'description',content:"UnifyCloud's Generative AI Data Readiness Assessment evaluates your data's readiness for AI applications. Discover gaps, enhancements, and a roadmap for optimal data readiness."});
     this.meta.addTag({name:'keywords',content:'Generative AI Assessment'});
+
+    this.meta.addTag({ property: 'og:locale', content: 'en_US' });
+    this.meta.addTag({ property: 'og:type', content: 'website' });
+    this.meta.addTag({ property: 'og:title', content: 'Generative AI Data Readiness Assessment Services - UnifyCloud' });
+    this.meta.addTag({ property: 'og:description', content: `UnifyCloud's Generative AI Data Readiness Assessment evaluates your data's readiness for AI applications. Discover gaps, enhancements, and a roadmap for optimal data readiness.` });
+    this.meta.addTag({ property: 'og:url', content: 'https://www.unifycloud.in/assessments/generative-ai-data-readiness-assessment' });
+    this.meta.addTag({ property: 'og:site_name', content: 'Generative AI Data Readiness Assessment Services - UnifyCloud' });
+    this.meta.addTag({ property: 'og:image', content: 'https://www.unifycloud.in/assets/img/gen-AI-og.jpg' });
+    this.meta.addTag({ property: 'og:image:width', content: '500' });
+    this.meta.addTag({ property: 'og:image:height', content: '300' });
+    this.meta.addTag({ property: 'og:image:alt', content: 'Gen AI Data Readiness Assessment' });
+
+    this.meta.addTag({ property: 'twitter:card', content: 'summary' });
+    this.meta.addTag({ property: 'twitter:description', content: `UnifyCloud's Generative AI Data Readiness Assessment evaluates your data's readiness for AI applications. Discover gaps, enhancements, and a roadmap for optimal data readiness.` });
+    this.meta.addTag({ property: 'twitter:title', content: 'Generative AI Data Readiness Assessment Services - UnifyCloud' });
+    this.meta.addTag({ property: 'twitter:site', content: '@unifycloudin' });
+    this.meta.addTag({ property: 'twitter:image', content: 'https://www.unifycloud.in/assets/img/unifycloudlogo.svg' });
+    this.meta.addTag({ property: 'twitter:creator', content: '@unifycloudin' });
   }
 }
--- src/app/home/components/assessments/components/responsive-ai-assessment/responsive-ai-assessment.component.ts
+++ src/app/home/components/assessments/components/responsive-ai-assessment/responsive-ai-assessment.component.ts
@@ -11,5 +11,24 @@ export class ResponsiveAiAssessmentComponent {
     this.titleSrc.setTitle('Responsible AI Assessment Services by UnifyCloud: Ethical AI Solutions');
     this.meta.addTag({name:'description',content:"Discover Responsible AI assessment Services by UnifyCloud, offering comprehensive ethical AI solutions. Our kit includes governance tools, compliance frameworks, and ethical AI algorithms for transparent and accountable AI deployment."});
     this.meta.addTag({name:'keywords',content:'Responsible AI Assessment Services'});
+
+    this.meta.addTag({ property: 'og:locale', content: 'en_US' });
+    this.meta.addTag({ property: 'og:type', content: 'website' });
+    this.meta.addTag({ property: 'og:title', content: 'Building a Responsible AI Implementation Framework for Generative AI' });
+    this.meta.addTag({ property: 'og:description', content: 'Discover a comprehensive Responsible AI Implementation Framework tailored for Generative AI, ensuring ethical algorithms, transparent decision-making, and sustainable AI outcomes. Learn key strategies and best practices for deploying responsible AI in your organization.' });
+    this.meta.addTag({ property: 'og:url', content: 'https://www.unifycloud.in/assessments/responsible-ai-assessment-services' });
+    this.meta.addTag({ property: 'og:site_name', content: 'Building a Responsible AI Implementation Framework for Generative AI' });
+    this.meta.addTag({ property: 'og:image', content: 'https://www.unifycloud.in/assets/img/res-AI-og.jpg' });
+    this.meta.addTag({ property: 'og:image:width', content: '500' });
+    this.meta.addTag({ property: 'og:image:height', content: '300' });
+    this.meta.addTag({ property: 'og:image:alt', content: 'Responsible AI Assessment' });
+
+    this.meta.addTag({ property: 'twitter:card', content: 'summary' });
+    this.meta.addTag({ property: 'twitter:description', content: 'Discover a comprehensive Responsible AI Implementation Framework tailored for Generative AI, ensuring ethical algorithms, transparent decision-making, and sustainable AI outcomes. Learn key strategies and best practices for deploying responsible AI in your organization.' });
+    this.meta.addTag({ property: 'twitter:title', content: 'Building a Responsible AI Implementation Framework for Generative AI' });
+    this.meta.addTag({ property: 'twitter:site', content: '@unifycloudin' });
+    this.meta.addTag({ property: 'twitter:image', content: 'https://www.unifycloud.in/assets/img/unifycloudlogo.svg' });
+    this.meta.addTag({ property: 'twitter:creator', content: '@unifycloudin' });
+
   }
 }
