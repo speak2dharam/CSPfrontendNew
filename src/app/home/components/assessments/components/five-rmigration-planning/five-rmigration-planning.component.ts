e45f2d45bbda3a7401911aee82aef7ee69c0cee9
Dharam <speak2dharam@gmail.com>
2024-06-13 10:54:06 +0530

    content update2

--- src/app/home/components/assessments/components/five-rmigration-planning/five-rmigration-planning.component.ts
+++ src/app/home/components/assessments/components/five-rmigration-planning/five-rmigration-planning.component.ts
@@ -8,8 +8,8 @@ import { Meta, Title } from '@angular/platform-browser';
 })
 export class FiveRMigrationPlanningComponent {
   constructor(private titleSrc:Title,private meta:Meta){
-    this.titleSrc.setTitle('Optimizing Migration Efforts: Unleashing the Power of 5R Wave Planning Assessment with UnifyCloud India');
-    this.meta.addTag({name:'description',content:'Unlock the full potential of your migration project with our comprehensive guide on 5R Wave Planning Assessment. Streamline your migration efforts and achieve seamless transitions with strategic planning and optimization strategies.'});
+    this.titleSrc.setTitle('Optimizing Migration Efforts: Unleashing the Power of 6R Wave Planning Assessment with UnifyCloud India');
+    this.meta.addTag({name:'description',content:'Unlock the full potential of your migration project with our comprehensive guide on 6R Wave Planning Assessment. Streamline your migration efforts and achieve seamless transitions with strategic planning and optimization strategies.'});
     this.meta.addTag({name:'keywords',content:''});
   }
 }
